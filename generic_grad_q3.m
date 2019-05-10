6b088e35cfdc24d6e72225c487bd0b3a3a5b1168
OS\Shubi <omer.shubi@gmail.com>
2019-05-10 22:13:44 +0300

    FIrst try

--- generic_grad_q3.m
+++ generic_grad_q3.m
@@ -1,48 +1,53 @@
-function [x ,x_1,x_10,x_100,x_1000, fs , gs , ts] = generic_grad_q3(f , gf , lsearch , x0 , MAX_ITERATIONS)
-
+function [x ,x_1,x_10,x_100,x_1000] = generic_grad_q3(f , gf , lsearch , x0 , MAX_ITERATIONS)
 
 % Input checks
 if ~isa(f,'function_handle') || ~isa(gf,'function_handle') || ~isa(lsearch,'function_handle')
 	error("f, gf and lsearch  must be functions")
 end
-if eps<=0 || ~isreal(eps) || ~isscalar(eps)
-        error("eps must be a real positive scalar")
+if ~isscalar(MAX_ITERATIONS) || ~(MAX_ITERATIONS==floor(MAX_ITERATIONS)) || ~isreal(MAX_ITERATIONS)
+    error("MAX_ITERATIONS must be a real scalar integer")
 end
 if ~isreal(x0)
     error("x0 must be real")
 end
-% Start Timer
-tic
 
 % Initialize Paramaters
 x = x0;
 grad = gf(x);
-grad_norm = norm(grad);
-
 % Result containers
-gs = grad_norm;
-fs = f(x0);
-ts = 0;
+
 iteration = 0;
+
 % Execute Generic Gradient Descent Algorithm
 % while (grad_norm > eps && iteration < MAX_ITERATIONS)
 while (iteration < MAX_ITERATIONS)
-
+tic
     t_k = lsearch(f,x,grad);
     x = x - (t_k * grad);
     grad = gf(x);
-    %grad_norm = norm(grad);
     
     % Recording interim results
-    fs = [fs, f(x)];
-    gs = [gs, grad_norm];
-    ts = [ts, toc]; 
     iteration=iteration+1;
     if(iteration == 1)
         x_1=x;
-        
+        disp(['Iteration ', num2str(iteration), ' complete'])
     end
+    if(iteration == 10)
+        x_10=x;
+        disp(['Iteration ', num2str(iteration), ' complete'])
+    end
+    if(iteration == 100)
+     x_100=x;
+    end
+    if(iteration == 1000)
+     x_1000=x;
+    end
+    
+    if(mod(iteration,100) == 0)
+        disp(['Iteration ', num2str(iteration), ' complete'])
+    end
+ 
 end
-iteration
+disp('Generic Grad loop complete. Time taken:')
 end
 
--- q3.m
+++ q3.m
@@ -1,20 +1,73 @@
+% Basic Example
 % sweden = [1,2,1,1,1;1,2,1,1,1;2,2,2,2,2;1,2,1,1,1;1,2,1,1,1];
 % colormap(parula)
 % imagesc(sweden)
 
 [A, b , x ] = blur ( 256 , 5 , 1 ) ;
-colormap ( gray )
-imagesc ( reshape (x , 256 , 256 ) )
-figure()
-colormap ( gray )
-imagesc ( reshape (b , 256 , 256 ) )
-figure()
+
+% Display the original and blurred images
+% colormap ( gray )
+% imagesc ( reshape (x , 256 , 256 ) )
+% figure()
+% colormap ( gray )
+% imagesc ( reshape (b , 256 , 256 ) )
 
 f = @(x) (norm(A*x-b))^2;
-Y = 2*(A'*A);
+
+% Cmputing gradient elements beforehand
+Y1 =A'*A;
+Y = 2*Y1;
 Z = 2*A'*b;
 g = @(x) Y*x-Z; %2*A'*(A*x-b)
+
 x0 = zeros(size(x));
-x_exact = generic_grad_q3(f, g, exact_quad(A), x0,1000);
+tic;
+% ***  Part 1  ***
+[~ ,x_1,x_10,x_100,x_1000] = generic_grad_q3(f, g, exact_quad_q3(Y), x0,1000);
+toc
+
+figure('Name','Image after 1 Iteration of GD with Exact Quad');
 colormap ( gray )
-imagesc ( reshape (x_exact , 256 , 256 ) )
+imagesc ( reshape (x_1 , 256 , 256 ) );
+
+
+figure('Name','Image after 10 Iterations of GD with Exact Quad');
+colormap ( gray )
+imagesc ( reshape (x_10 , 256 , 256 ) );
+
+
+figure('Name','Image after 100 Iterations of GD with Exact Quad');
+colormap ( gray )
+imagesc ( reshape (x_100 , 256 , 256 ) );
+
+
+figure('Name','Image after 1000 Iterations of GD with Exact Quad');
+colormap ( gray )
+imagesc ( reshape (x_1000 , 256 , 256 ) );
+
+
+% ***  Part 2  ***
+L = 2*max(eigs(Y1));
+tic;
+[~ ,x_1,x_10,x_100,x_1000] = generic_grad_q3(f, g, const_step_q3(1/L), x0, 1000);
+toc
+
+figure('Name','Image after 1 Iteration of GD with Const Step');
+colormap ( gray )
+imagesc ( reshape (x_1 , 256 , 256 ) );
+
+
+figure('Name','Image after 10 Iterations of GD with Const Step');
+colormap ( gray )
+imagesc ( reshape (x_10 , 256 , 256 ) );
+
+
+figure('Name','Image after 100 Iterations of GD with Const Step');
+colormap ( gray )
+imagesc ( reshape (x_100 , 256 , 256 ) );
+
+
+figure('Name','Image after 1000 Iterations of GD with Const Step');
+colormap ( gray )
+imagesc ( reshape (x_1000 , 256 , 256 ) );
+
